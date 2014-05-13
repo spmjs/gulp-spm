ef874de5484205344e4ec715e80b06071c7d2bc5
popomore <sakura9515@gmail.com>
2014-05-13 17:39:42 +0800

    move util

---
+++ test/util.test.js
@@ -0,0 +1,90 @@
+'use strict';
+
+var util = require('..').util;
+
+describe('Util', function() {
+
+  it('template', function() {
+    util.template().should.eql('');
+    util.template('{{a}}/{{b}}', {
+      a: 1
+    }).should.eql('1/');
+  });
+
+  it('extendOption', function() {
+    util.extendOption().should.eql({
+      ignore: [],
+      idleading: '{{name}}/{{version}}',
+      cwd: process.cwd(),
+      pkg: null,
+      rename: null
+    });
+
+    var orig = {
+      pkg: {},
+      ignore: null
+    };
+    util.extendOption(orig).should.eql({
+      pkg: {},
+      ignore: [],
+      idleading: '{{name}}/{{version}}',
+      cwd: process.cwd(),
+      rename: null
+    });
+    orig.should.eql({
+      pkg: {},
+      ignore: null
+    });
+  });
+
+  it('hideExt', function() {
+    util.hideExt('a.js').should.eql('a');
+    util.hideExt('./a.js').should.eql('./a');
+    util.hideExt('./a').should.eql('./a');
+    util.hideExt('./a.css').should.eql('./a.css');
+  });
+
+  it('addExt', function() {
+    util.addExt('a').should.eql('a.js');
+    util.addExt('a.js').should.eql('a.js');
+    util.addExt('./a.js').should.eql('./a.js');
+    util.addExt('./a.css').should.eql('./a.css');
+  });
+
+  it('rename', function() {
+    util.rename('a.js').should.eql('a.js');
+    util.rename('a.js', {
+      rename: {suffix: '-debug'}
+    }).should.eql('a-debug.js');
+    util.rename('a', {
+      rename: function(file) {
+        file.basename += '-debug';
+        return file;
+      }
+    }).should.eql('a-debug.js');
+    util.rename('a', {
+      rename: function() {
+        throw new Error();
+      }
+    }).should.eql('a.js');
+  });
+
+  it('isRelative', function() {
+    util.isRelative('./a').should.be.ok();
+    util.isRelative('../a').should.be.ok();
+    util.isRelative('/a.js').should.not.be.ok();
+    util.isRelative('a.js').should.not.be.ok();
+  });
+
+  it('resolvePath', function() {
+    util.resolvePath('./a.js', 'b.js').should.eql('a.js');
+    util.resolvePath('./a', 'b.js').should.eql('a');
+    util.resolvePath('../a.js', 'src/b.js').should.eql('a.js');
+    util.resolvePath('a.js', 'b/c/d.js').should.eql('a.js');
+    util.resolvePath('a.js').should.eql('a.js');
+    (function() {
+      util.resolvePath('../a.js', 'b.js');
+    }).should.throw('../a.js is out of bound');
+  });
+
+});
