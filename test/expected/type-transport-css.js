30223668a1deffec32ac055ca87e297fe24f25e3
popomore <sakura9515@gmail.com>
2014-06-07 02:50:35 +0800

    use css2str

--- test/expected/type-transport-css.js
+++ test/expected/type-transport-css.js
@@ -1,3 +1,3 @@
 define("type-transport/1.0.0/a.css.js", [], function(require, exports, module){
-seajs.importStyle('body{  color: #fff;  border-color: transparent\\0;}');
+seajs.importStyle('body{color:#fff;border-color:transparent\\0;}');
 });
