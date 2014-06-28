683672f38e80a0e8aefd9b21e5020aeeec1798f2
popomore <sakura9515@gmail.com>
2014-06-29 01:09:23 +0800

    add transport

---
+++ test/expected/transport-relative.js
@@ -0,0 +1,41 @@
+define("type-transport/1.0.0/index", ["type-transport/1.0.0/a.css.js","type-transport/1.0.0/a.json","type-transport/1.0.0/a.tpl","type-transport/1.0.0/a.handlebars","type-transport/1.0.0/a","handlebars-runtime/1.3.0/handlebars"], function(require, exports, module){
+require("type-transport/1.0.0/a.css.js");
+require("type-transport/1.0.0/a.json");
+require("type-transport/1.0.0/a.tpl");
+require("type-transport/1.0.0/a.handlebars");
+require("type-transport/1.0.0/a");
+
+});
+define("type-transport/1.0.0/a.css.js", ["import-style/1.0.0/index"], function(require, exports, module){
+require("import-style/1.0.0/index")('body{  color: #fff;  border-color: transparent\\0;}');
+
+});
+define("type-transport/1.0.0/a.json", [], function(require, exports, module){
+module.exports = {}
+
+});
+define("type-transport/1.0.0/a.tpl", [], function(require, exports, module){
+module.exports='<div></div>';
+
+});
+define("type-transport/1.0.0/a.handlebars", ["handlebars-runtime/1.3.0/handlebars"], function(require, exports, module){
+var Handlebars = require("handlebars-runtime/1.3.0/handlebars")["default"];
+module.exports = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression;
+
+
+  buffer += "<div>";
+  if (helper = helpers.content) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.content); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "</div>\n";
+  return buffer;
+  });
+
+});
+define("type-transport/1.0.0/a", [], function(require, exports, module){
+console.log('a');
+
+});
---
+++ test/transport.test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+require('should');
+var fs = require('fs');
+var join = require('path').join;
+var gulp = require('gulp');
+var Package = require('father').SpmPackage;
+var base = join(__dirname, 'fixtures');
+var transport = require('../lib/transport');
+
+describe.only('Transport', function() {
+
+ it('transport relative', function(done) {
+    var pkg = getPackage('type-transport', {
+      extraDeps: {
+        handlebars: 'handlebars-runtime',
+        css: 'import-style'
+      }
+    });
+
+    var opt = {
+      cwd: join(base, 'type-transport'),
+      cwdbase: true
+    };
+    gulp.src('index.js', opt)
+    .pipe(transport({pkg: pkg}))
+    .on('data', function(file) {
+      assert(file, 'transport-relative.js');
+    })
+    .on('end', done);
+  });
+
+});
+
+function getPackage(name, options) {
+  var dir = join(base, name);
+  return new Package(dir, options);
+}
+
+function assert(file, expectedFile) {
+  var code = file.contents.toString();
+  var expected = readFile(__dirname + '/expected/' + expectedFile);
+  code.should.eql(expected);
+}
+
+function readFile(path) {
+  return fs.readFileSync(path).toString().replace(/\r/g, '');
+}
