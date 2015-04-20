4cfe5b4efc0587848e561aceb0bb8e5ee1abaf83
popomore <sakura9515@gmail.com>
2015-04-20 19:10:17 +0800

    feat: add styleBox as an option

---
+++ test/expected/transport-stylebox.js
@@ -0,0 +1,10 @@
+define("type-transport/1.0.0/stylebox", ["import-style/1.0.0/index"], function(require, exports, module){
+require("type-transport/1.0.0/stylebox.css.js");
+module.exports.outerBoxClass="type-transport-1_0_0";
+
+});
+define("type-transport/1.0.0/stylebox.css.js", ["import-style/1.0.0/index"], function(require, exports, module){
+require("import-style/1.0.0/index")('@charset "utf-8";.type-transport-1_0_0  body{_color:red;}.type-transport-1_0_0  .content li,.type-transport-1_0_0  .content a{*display:inline-block;}@media screen{.type-transport-1_0_0  body{font-size:13px;}.type-transport-1_0_0  .content li{background-color:#fff;}}@keyframes foo{0%{top:0;left:0;}100%{top:100px;left:100%;}}:root .type-transport-1_0_0   .content{font-size:12px;}');
+module.exports.outerBoxClass="type-transport-1_0_0";
+
+});
